71f90f9bb35f50b1d070c192d71dba82ddb391a6
Vivek Dubey <vivek@Viveks-MacBook-Air.local>
2018-11-06 04:36:07 +0530

    added dynamic xml runner class

---
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,22 @@
+package com.qa.tests;
+
+import java.util.Hashtable;
+
+import org.testng.annotations.Test;
+
+import com.qa.base.TestBase;
+import com.qa.pages.HomePage;
+
+public class LoginPageTest extends TestBase{
+
+	
+	@Test(dataProvider="dataProvider")
+	public void loginPageTitleTest(Hashtable<String,String> data) {
+		initial_test_tasks(data);
+		HomePage hp=new HomePage();
+		hp.clickLink("PEOPLE");
+		//hp.nav_to_peoplePage();
+	}
+	
+
+}
---
+++ src/test/java/com/qa/tests/runner.java
@@ -0,0 +1,62 @@
+package com.qa.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
+import com.qa.util.FiloExcelReader;
+
+
+public class runner {
+
+	public  XmlTest test;
+	public  List<XmlClass> classes;
+	public  List<XmlSuite> suites;
+	public  List<XmlClass> classList;
+	public  List<XmlSuite> suiteList;
+	public  TestNG tng;
+	public  XmlSuite suite;
+	public String Sheetname;
+	
+	
+	public void test_runner(){
+	
+
+		
+	
+		suite = new XmlSuite();
+		suite.setName("Portal Automation Testing");
+		suite.addListener("com.qa.ExtentReportListener.ExtentReporterNG" );
+		
+		
+		for(String tcid:FiloExcelReader.getAllTestClasses()) {
+			System.out.println(tcid);
+			test = new XmlTest(suite);
+			test.setName(tcid.toString().trim());
+			classes = new ArrayList<XmlClass>();
+			classes.add(new XmlClass("com.qa.testcases."+tcid));
+			test.setXmlClasses(classes);
+		}
+		
+		
+		suites = new ArrayList<XmlSuite>();
+		suites.add(suite);
+		tng = new TestNG();
+		tng.setXmlSuites(suites);
+		tng.run();
+
+
+	}
+	
+	public static void main(String[] args) {
+		runner r=new runner();
+		r.test_runner();
+	}
+	
+}
+
+
